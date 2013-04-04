191fc5162e3de91283d761f7901f8f8e280db8d7
Alexander Klimetschek <aklimets@adobe.com>
2013-04-05 00:56:22 +0200

    improving google calculator
    - not choking over special 0x00A0 thousand delimiter character
    - properly parsing "fake" json via yaml
    - better error display

--- google-calculator/script.py
+++ google-calculator/script.py
@@ -1,42 +1,66 @@
-import httplib
+import sys
 import urllib
-import json
-import re
-from xml.dom.minidom import Document
+import urllib2
+import yaml
+from xml.sax.saxutils import escape
 
-q = "{query}"
-params = urllib.urlencode({"q": q})
-conn = httplib.HTTPConnection("www.google.com", 80)
-conn.request("GET", "/ig/calculator?hl=en&amp;"+params)
-response = conn.getresponse()
-data = response.read()
-conn.close()
+xml_tpl = '''<?xml version="1.0"?>
+<items>
+  <item uid="%(uid)s" arg="%(arg)s" valid="YES">
+    <title>%(title)s</title>
+    <subtitle>%(subtitle)s</subtitle>
+    <icon>icon.png</icon>
+  </item>
+</items>'''
 
-data = re.sub(r"{\s*(\w)", r'{"\1', data)
-data = re.sub(r",\s*(\w)", r',"\1', data)
-data = re.sub(r"(\w):", r'\1":', data)
+def xml(uid, arg, title, subtitle=""):
+    return xml_tpl % ({"uid": escape(uid), "arg": escape(arg), "title": title, "subtitle": subtitle})
+    
 
-dataobj = json.loads(data)
-doc = Document()
-items = doc.createElement("items")
-xmlitem = doc.createElement("item")
-if dataobj["error"] == "":
-	output = dataobj["lhs"] + " = " + dataobj["rhs"]
-else:
-	output = "Error"
-xmlitem.setAttribute("uid", dataobj["lhs"])
-xmlitem.setAttribute("arg", dataobj["rhs"])
-xmlitem.setAttribute("valid", "YES")
-attr = doc.createElement("title")
-attr.appendChild(doc.createTextNode(output))
-xmlitem.appendChild(attr)
-attr = doc.createElement("subtitle")
-attr.appendChild(doc.createTextNode(q))
-xmlitem.appendChild(attr)
-attr = doc.createElement("icon")
-attr.appendChild(doc.createTextNode("icon.png"))
-xmlitem.appendChild(attr)
-items.appendChild(xmlitem)
+try:
+    q = "{query}"
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    
+    params = urllib.urlencode({"q": q})
+    # faking browser user agent necessary to get back utf-8 encoding
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
+    request = urllib2.Request("http://www.google.com/ig/calculator?hl=en&" + params, None, headers)
+    response = urllib2.urlopen(request).read()
+    response = response.decode("utf-8")
+    # replace magic thousand delimiter char
+    response = response.replace(u"\u00A0", ".")
+    
+    data = yaml.load(response)
+    
+    error = data["error"]
+    exp = data["lhs"]
+    result = data["rhs"]
+    
+    title = q
+    if error == "":
+        result = result.replace("<sup>", "^").replace("</sup>", "")
+        title = result
+        subtitle = exp + " = " + result
+    else:
+        try:
+            errorcode = int(error)
+        except:
+            errorcode = -1
+        
+        if errorcode == -1:
+            #subtitle = "Oh, Google says: " + error
+            subtitle = "..."
+        elif errorcode == 0:
+            subtitle = q
+            #subtitle = "Please complete expression..."
+        elif errorcode == 4:
+            subtitle = "..."
+            #subtitle = "Sorry, cannot calculate that..."
+        else:
+            subtitle = "Google reported error code " + str(errorcode)
 
-doc.appendChild(items)
-print unicode(doc.toxml()).encode("utf-8")
+    print xml("calc {query}", result, title, subtitle)
+
+except BaseException, e:
+    print xml("calc {query}", "error", title, "Unexpected error", str(e))
